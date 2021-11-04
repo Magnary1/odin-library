82b35798ee289dfd51e1edb807edc060bc1b81f9
jonathan <jonjonnjonjonn@gmail.com>
2021-11-03 21:56:37 -0500

    loops through array now to add book. need to delete from array as well

--- script.js
+++ script.js
@@ -33,35 +33,26 @@ function addBookToLibrary() {
     )
     myLibrary.push(newBook)
 
-    let tableRow = document.createElement(`tr`)
-    let tableTitle = document.createElement(`td`)
-    let tableAuthor = document.createElement(`td`)
-    let tablePages = document.createElement(`td`)
-    let tableRead = document.createElement(`td`)
-    let tableDelete = document.createElement(`td`)
-    let deleteButton = document.createElement(`button`)
-
-    // tableRow.textContent = myLibrary.at(-1).title
-    tableTitle.textContent = myLibrary.at(-1).title
-    tableAuthor.textContent = myLibrary.at(-1).author
-    tablePages.textContent = myLibrary.at(-1).pages
-    tableRead.textContent = myLibrary.at(-1).read
 
 
 
-    deleteButton.textContent = `X`
-    deleteButton.classList.add(`delete-button`)
-    tableDelete.append(deleteButton)
-
-    tableRow.append(tableTitle)
-    tableRow.append(tableAuthor)
-    tableRow.append(tablePages)
-    tableRow.append(tableRead)
-    tableRow.append(tableDelete)
+    // ADDS BOOK TO THE LAST ROW OF TABLE
+    const tableRow = document.createElement(`tr`)
+    for (let x in myLibrary.at(-1)) {
+        const tableData = document.createElement(`td`)
+        tableData.textContent = myLibrary.at(-1)[x]
+        tableRow.append(tableData)
+    }
     table.append(tableRow)
 
 
 
+    let tableDelete = document.createElement(`td`)
+    let deleteButton = document.createElement(`button`)
+    deleteButton.textContent = `X`
+    deleteButton.classList.add(`delete-button`)
+    tableDelete.append(deleteButton)
+    tableRow.append(tableDelete)
     let deleteButtons = document.querySelectorAll(`.delete-button`)
     deleteButtons.forEach(button => button.addEventListener(`click`, deleteBook))
 }
@@ -77,7 +68,46 @@ function deleteBook(e) {
     e.target.parentElement.parentElement.remove()
 }
 
+function firstAttemptOfAddingTable() {
+        //MANUALLY GOES THROUGH EACH INPUT AND ADDS TO TABLE
 
+        let tableRow = document.createElement(`tr`)
+        let tableTitle = document.createElement(`td`)
+        let tableAuthor = document.createElement(`td`)
+        let tablePages = document.createElement(`td`)
+        let tableRead = document.createElement(`td`)
+        let tableDelete = document.createElement(`td`)
+        let deleteButton = document.createElement(`button`)
+    
+        tableTitle.textContent = myLibrary.at(-1).title
+        tableAuthor.textContent = myLibrary.at(-1).author
+        tablePages.textContent = myLibrary.at(-1).pages
+        tableRead.textContent = myLibrary.at(-1).read
+    
+    
+    
+        deleteButton.textContent = `X`
+        deleteButton.classList.add(`delete-button`)
+        tableDelete.append(deleteButton)
+    
+        tableRow.append(tableTitle)
+        tableRow.append(tableAuthor)
+        tableRow.append(tablePages)
+        tableRow.append(tableRead)
+        tableRow.append(tableDelete)
+        table.append(tableRow)
+}
 
-
-
+function addWholeLibrary() {
+        // ADDS WHOLE LIBRARY ARRAY TO TABLE
+        for (let x in myLibrary) {
+            const tableRow = document.createElement(`tr`)
+            for (let y in myLibrary[x]) {
+                const tableData = document.createElement(`td`)
+                tableData.textContent = myLibrary[x][y]
+                tableRow.append(tableData)
+            }
+            table.append(tableRow)
+        }
+    
+}
